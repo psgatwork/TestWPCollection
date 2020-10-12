de93ba7d18b034fe7f9e212f368724648e0a408f
psgatwork <psgatwork@gmail.com>
2020-10-12 13:41:31 +0530

    added image with buttons webpart

---
+++ src/webparts/imageWithButtons/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IImageWithButtonsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'ImageWithButtonsWebPartStrings' {
+  const strings: IImageWithButtonsWebPartStrings;
+  export = strings;
+}
