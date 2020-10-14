11cd30a5442566147b392e216d4109387c0d213b
psgatwork <psgatwork@gmail.com>
2020-10-14 14:48:30 +0530

    Image braking and initial conf complete

--- src/webparts/imageWithButtons/components/ImageWithButtons.tsx
+++ src/webparts/imageWithButtons/components/ImageWithButtons.tsx
@@ -2,24 +2,63 @@ import * as React from 'react';
 import styles from './ImageWithButtons.module.scss';
 import { IImageWithButtonsProps } from './IImageWithButtonsProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import { PrimaryButton } from 'office-ui-fabric-react';
+
 
 export default class ImageWithButtons extends React.Component<IImageWithButtonsProps, {}> {
   public render(): React.ReactElement<IImageWithButtonsProps> {
     return (
-      <div className={ styles.imageWithButtons }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
+      <div className={styles.imageWithButtons}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.column}>
+              <div className={styles.baseContainer}>
+                <div className={styles.imageContainer}>
+                  <img className={styles.theImage} src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg"></img>
+                </div>
+                <div className={styles.buttonContainer}>
+                  <PrimaryButton text="Learn" onClick={this._alertClicked} allowDisabledFocus />
+                </div>
+              </div>
+            </div>
+            <div className={styles.column}>
+              <div  className={styles.baseContainer}>
+                <div className={styles.imageContainer} >
+                  <img className={styles.theImage} src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg"></img>
+                </div>
+                <div  className={styles.buttonContainer}>
+                  <PrimaryButton text="Learn" onClick={this._alertClicked} allowDisabledFocus />
+                </div>
+              </div>
+            </div>
+            <div className={styles.column}>
+              <div  className={styles.baseContainer}>
+                <div className={styles.imageContainer} >
+                  <img className={styles.theImage} src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg"></img>
+                </div>
+                <div  className={styles.buttonContainer}>
+                  <PrimaryButton text="Learn" onClick={this._alertClicked} allowDisabledFocus />
+                </div>
+              </div>
+            </div>
+            <div className={styles.column}>
+              <div  className={styles.baseContainer}>
+                <div className={styles.imageContainer} >
+                  <img className={styles.theImage} src="https://cdn.pixabay.com/photo/2015/12/01/20/28/road-1072823__340.jpg"></img>
+                </div>
+                <div  className={styles.buttonContainer}>
+                  <PrimaryButton text="Learn" onClick={this._alertClicked} allowDisabledFocus />
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
+
     );
+  }
+
+  private _alertClicked(): void {
+    console.log('bt c');
   }
 }
